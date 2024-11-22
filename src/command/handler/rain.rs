970b7736754c72464fc5b09c153cfe9a859a2430
Tianyi Ma <tn.ma@outlook.com>
2024-11-21 20:43:34 -0800

    Remove debug chat message from !rain

--- src/command/handler/rain.rs
+++ src/command/handler/rain.rs
@@ -8,20 +8,18 @@ static TAG: &str = "rain";
 
 pub fn handle(server: &mut Server, game: &mut Game, command_ctx: CommandContext) {
     let Some(player_actor) = game.actor_manager.get_player_actor(&command_ctx.sender) else {
-        let _ = server.send_chat_message(&command_ctx.sender, "Failed. No Player character found.");
+        server.send_chat_message(&command_ctx.sender, "Failed. No Player character found.");
         return;
     };
-    let _ = server.send_chat_message(&command_ctx.sender, &format!("{:?}", player_actor.position));
 
     // TODO: We should actually check if the server host SteamId can create the actor
     if !game
         .actor_manager
         .user_can_create_actor(&command_ctx.sender, true, &ActorType::Raincloud)
     {
-        let _ = server.send_chat_message(&command_ctx.sender, "Failed. You have too many props!");
+        server.send_chat_message(&command_ctx.sender, "Failed. You have too many props!");
         println!(
-            "[{}] User cannot create raincloud actor: steam_id = {}",
-            TAG,
+            "[{TAG}] User cannot create raincloud actor: steam_id = {}",
             command_ctx.sender.raw()
         );
         return;
@@ -29,7 +27,10 @@ pub fn handle(server: &mut Server, game: &mut Game, command_ctx: CommandContext)
 
     let spawn_manager = &mut game.spawn_manager;
     if !spawn_manager.can_spawn_user_actor(&ActorType::Raincloud) {
-        let _ = server.send_chat_message(&command_ctx.sender, "Failed. Someone already spawned a rain cloud.");
+        let _ = server.send_chat_message(
+            &command_ctx.sender,
+            "Failed. Someone already spawned a rain cloud.",
+        );
         return;
     }
     let mut raincloud_position = player_actor.position.clone();
@@ -41,5 +42,5 @@ pub fn handle(server: &mut Server, game: &mut Game, command_ctx: CommandContext)
         &raincloud_position,
     );
 
-    let _ = server.send_chat_message(&command_ctx.sender, "Spawned rain cloud.");
+    server.send_chat_message(&command_ctx.sender, "Spawned rain cloud.");
 }
