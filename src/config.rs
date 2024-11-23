1ddabfa78891b52eefeb7932156b410f3253cafa
Tianyi Ma <tn.ma@outlook.com>
2024-11-22 18:09:03 -0800

    Make config values optional

--- src/config.rs
+++ src/config.rs
@@ -2,25 +2,54 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
+    #[serde(default = "default_name")]
     pub name: String,
+    #[serde(default = "default_motd")]
     pub motd: String,
+    #[serde(default = "default_game_version")]
     pub game_version: String,
+    #[serde(default = "default_max_players")]
     pub max_players: u32,
+    #[serde(default = "default_code_only")]
     pub code_only: bool,
+    #[serde(default = "default_adult_only")]
     pub adult_only: bool,
-    pub ban_list: Vec<u64>
+    #[serde(default = "default_ban_list")]
+    pub ban_list: Vec<u64>,
 }
 
 impl Default for Config {
     fn default() -> Self {
         Config {
-            name: "A Ducky Server".to_string(),
-            motd: "This lobby is powered by Ducky.\nType !help to see commands.".to_string(),
-            game_version: "1.1".to_string(),
-            max_players: 25,
-            code_only: true,
-            adult_only: false,
-            ban_list: vec![],
+            name: default_name(),
+            motd: default_motd(),
+            game_version: default_game_version(),
+            max_players: default_max_players(),
+            code_only: default_code_only(),
+            adult_only: default_adult_only(),
+            ban_list: default_ban_list(),
         }
     }
+}
+
+fn default_name() -> String {
+    "A Ducky Server".to_string()
+}
+fn default_motd() -> String {
+    "This lobby is powered by Ducky.\nType !help to see commands.".to_string()
+}
+fn default_game_version() -> String {
+    "1.1".to_string()
+}
+fn default_max_players() -> u32 {
+    12
+}
+fn default_code_only() -> bool {
+    true
+}
+fn default_adult_only() -> bool {
+    false
+}
+fn default_ban_list() -> Vec<u64> {
+    vec![]
 }
